e3d3aa18512ec6c4cbe53d427bde3d912babb0d7
Philipp Baumann <ph.baumann@t-online.de>
2019-10-29 11:08:02 +0100

    Merge draws with parallel execution of checkFun again. Before it has been seperated which is not convenient anymore if multiple locations of the spline are tested

--- R/sampling_functions.R
+++ R/sampling_functions.R
@@ -23,20 +23,17 @@ gen_samples <- function(
   n_cores = detectCores(logical = FALSE),
   app = "")
 {
-  if (init_draw == TRUE) {
-    if (set_seed == TRUE) set.seed(1)
-    fac <- sampFun(nrSample)
-    yb <- lapply(fac, function(tau) as.numeric(orthdir + tau*dir))
-    draw <- list("yb" = yb, "fac" = fac)
-    attr(draw,"seed") <- .Random.seed
-    attr(draw,"time") <- Sys.time()
-    attr(draw,"os_info") <- sessionInfo()
-    if (is.null(path)) stop("Specify path!")
-    save(draw, file = paste0(path,"PoSI/",app,"/y_draw_",app,".RData"))
-    stop("Draws saved. \n")
-  }
+  if (set_seed == TRUE) set.seed(1)
+  fac <- sampFun(nrSample)
+  yb <- lapply(fac, function(tau) as.numeric(orthdir + tau*dir))
+  draw <- list("yb" = yb, "fac" = fac)
+  attr(draw,"seed") <- .Random.seed
+  attr(draw,"time") <- Sys.time()
+  attr(draw,"os_info") <- sessionInfo()
+  if (is.null(path)) stop("Specify path!")
+  save(draw, file = paste0(path,"PoSI/",app,"/y_draw_",c(app,as.character(Sys.time())),".RData"))
 
-  cat("Parallel draw with checkFun exe active! \n")
+  cat("Parallel execution of checkFun active! \n")
   cluster_cl <- makeCluster(n_cores, outfile = "")
   clusterEvalQ(cluster_cl, {library(mgcv)
     library(cAIC4)})
@@ -49,7 +46,7 @@ gen_samples <- function(
     logvals <- parSapply(cluster_cl, yb[y_idx], checkFun)
     # logvals <- sapply(yb[y_idx], checkFun)
     stopCluster(cluster_cl)
-    cat("Parallel draw with checkFun exe and y_idx deactive! \n")
+    cat("Parallel execution of checkFun and y_idx deactive! \n")
     return(list(logvals = logvals, fac = fac))
   }
 
@@ -57,7 +54,7 @@ gen_samples <- function(
   yb <- lapply(fac, function(tau) as.numeric(orthdir + tau*dir))
   logvals <- parSapply(cluster_cl, yb, checkFun)
   stopCluster(cluster_cl)
-  cat("Parallel draw with checkFun exe deactive! \n")
+  cat("Parallel execution of checkFun deactive! \n")
   return(list(logvals = logvals, fac = fac))
 
 }
